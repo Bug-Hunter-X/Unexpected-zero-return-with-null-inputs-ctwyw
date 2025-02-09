8c04adf071969a1740310ea05f31cb6978e14f78
BugHunter-X <askzenai@gmail.com>
2025-02-09 01:08:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+function foo(a, b) {
+  if (a === null || b === null) {
+    return 0; 
+  } else {
+    return a + b; 
+  }
+}
+
+console.log(foo(null, 1)); // Output: 0
+console.log(foo(1, null)); // Output: 0
+console.log(foo(null, null)); // Output: 0
+console.log(foo(1, 2)); // Output: 3
